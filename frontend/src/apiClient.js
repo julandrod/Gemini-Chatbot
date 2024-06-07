8cfbfa9c89df38ce18ee8aea41b5d108fa39c540
julandrod <julian.andres.rodriguez@gmail.com>
2024-06-07 00:04:35 -0500

    fix backend url to the axios

--- frontend/src/apiClient.js
+++ frontend/src/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   withCredentials: true,
-  baseURL: "https://gemini-chatbot-e4tn.onrender.com/",
+  baseURL: "https://gemini-chatbot-e4tn.onrender.com/api/v1",
 });
 
 export default apiClient;
