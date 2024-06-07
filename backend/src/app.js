f2b9edd1e7f1cc48e73d18b0619feff02acc6edc
julandrod <julian.andres.rodriguez@gmail.com>
2024-06-06 23:21:07 -0500

    add frontend url to the cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { connectDB } from "./db/connect.js";
-import {options} from "./swagger-doc/options.js"
+import { options } from "./swagger-doc/options.js";
 
 // Import routes
 // const routes = require("./routes");
@@ -17,14 +17,14 @@ import { errorMiddleware, notFoundMiddleware } from "./middlewares/index.js";
 // API config
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(cors());
+app.use(cors({ origin: "https://gemini-chatbot-silk.vercel.app/", credentials: true }));
 app.use(express.json());
-app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev", { skip: (req, res) => process.env.NODE_ENV === "test" }));
 
 // API routes
 app.get("/", (req, res) => {
-  res.send("Tennis League");
+  res.send("Chatbot");
 });
 app.use("/api/v1", routeHandler);
 
@@ -40,9 +40,7 @@ app.use(errorMiddleware);
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
-      console.log(`Server start on port: ${port}`)
-    );
+    app.listen(port, () => console.log(`Server start on port: ${port}`));
   } catch (error) {
     console.log(error);
   }
